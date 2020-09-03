774c81f96d06d592b99d0e6530d2133c369ceb80
Alif Maulana <alifmaulana26@gmail.com>
2020-09-03 22:13:14 +0700

    sebelum login

---
+++ app/Controllers/Admin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+class Admin extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Dashboard',
+            'header' => 'Admin Dashboard'
+        ];
+        return view('admin/index', $data);
+    }
+
+    public function login()
+    {
+        $data = [
+            'title' => 'Login',
+            'header' => 'Admin Dashboard'
+        ];
+        return view('admin/login', $data);
+    }
+    public function signup()
+    {
+        $data = [
+            'title' => 'Login',
+            'header' => 'Admin Dashboard'
+        ];
+        return view('admin/register', $data);
+    }
+}
