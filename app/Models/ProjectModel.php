6d59a103689385e3cc90aa1b1c713f419cad0c90
Alif Maulana <alifmaulana26@gmail.com>
2020-09-19 08:22:59 +0700

    Buat model project

---
+++ app/Models/ProjectModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ProjectModel extends Model
+{
+    protected $table = 'project';
+    protected $useTimestamps = true;
+    protected $allowedFields = ['name', 'subname', 'overview', 'achievement', 'platform', 'size', 'link', 'logo', 'poster', ''];
+    public function getProject($id = false)
+    {
+        if ($id == false) {
+            return $this->findAll();
+        }
+        return $this->where(['id' => $id])->first();
+    }
+}
